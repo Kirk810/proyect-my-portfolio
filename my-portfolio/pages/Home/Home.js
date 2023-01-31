a5cfd036f247175090acb1c33847c897fa382a55
Adrián <123007421+Kirk810@users.noreply.github.com>
2023-01-31 16:06:43 +0100

    Update Home.js

--- my-portfolio/pages/Home/Home.js
+++ my-portfolio/pages/Home/Home.js
@@ -8,7 +8,7 @@ export const Home = () => {
     <section class="home">
     <p>Hey, I'm</p>
     <h1>Adrián Soto</h1>
-    <p>I am a student of the web development bootcamp in Madrid, Spain. Here I will share my projects and creations to make themselves known.</p>
+    <p>I am a student of the web development bootcamp in Neoland, Madrid. Here I will share my projects and creations to make themselves known.</p>
     <a href="mailto:adriansoto89@gmail.com">Say hi →</a>
     </section>`;
 };
